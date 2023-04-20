dea0b89b9e6855cb2eba74f7a4686f1814817277
Daniele Lenares <daniele@lenares.me>
2023-04-20 15:05:06 +0200

    fix: import got module with dynamic import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import https from 'https';
 import crypto from 'crypto';
-import got, { Got, Method } from 'got';
+import { Got, Method } from 'got';
 import { XMLParser } from 'fast-xml-parser';
 import { Collection, File, XMLBody } from './interfaces.js';
 
@@ -15,7 +15,8 @@ export default class nextdav {
   }
 
   private async getClient(): Promise<Got> {
-    return got.extend({
+    const gotModule = await import('got');
+    return gotModule.default.extend({
       headers: {
         Authorization: `Basic ${this.basicAuth}`,
       },
